667d95a08aac37629e73e5dfabc41ca062d8191e
Vikrant Mehendale <61665688+vikrantMehendale@users.noreply.github.com>
2020-04-09 20:20:34 -0700

    Add files via upload

---
+++ vendingmachin_Top.sv
@@ -0,0 +1,18 @@
+module TOP();
+				bit valid_s;
+				bit [2:0] items_s;
+				bit [3:0] count_s;
+				bit [7:0] cost_s;
+				bit enter_key;
+				logic clk,rst;
+				bit [1:0]coins;
+				bit  [5:0]button;
+				bit [2:0]product;
+				logic [1:0]status;
+				bit [7:0]balance;
+				bit [7:0]info;
+				
+				tb_vm DUT(.*);			//Instantiation Of Testbench of Vending Machine.
+				vm VM(.*);				//Instantiation of Module of Vending Machine.
+				
+endmodule
